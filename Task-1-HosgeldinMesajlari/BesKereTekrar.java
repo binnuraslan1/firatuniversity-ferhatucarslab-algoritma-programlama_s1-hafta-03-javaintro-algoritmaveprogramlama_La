70f261da87b6500de68da432ed9a19e6b5d23fee
binnuraslan1 <binnuraslan888@gmail.com>
2025-10-22 21:13:27 +0300

    BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,11 @@
+public class BesKereTekrar {
+    public static void main (String [] args) {
+
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+    }
+ }
+ 
