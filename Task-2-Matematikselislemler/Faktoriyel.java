821b002c72ac1bbb29abb584672a576729d85afb
binnuraslan1 <binnuraslan888@gmail.com>
2025-10-22 22:07:53 +0300

    Create ToplamaSeri

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,6 @@
+public class ToplamaSeri {
+    public static void main (String [] args) {
+        int sonuc = 1+2+3+4+5+6+7+8+9 ;
+        System .out.println(" 1+2+3+4+5+6+7+8+9 = " + sonuc);
+    }
+    }
