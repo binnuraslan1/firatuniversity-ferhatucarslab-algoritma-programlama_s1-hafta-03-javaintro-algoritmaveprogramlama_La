0d1026ab999488bc732b0b5c83e341bfbeb8952d
binnuraslan1 <binnuraslan888@gmail.com>
2025-10-22 22:27:44 +0300

    Faktöriyel.java

--- Task-2-Matematikselislemler/Faktoriyel.java
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -1,6 +1,9 @@
-public class ToplamaSeri {
-    public static void main (String [] args) {
-        int sonuc = 1+2+3+4+5+6+7+8+9 ;
-        System .out.println(" 1+2+3+4+5+6+7+8+9 = " + sonuc);
-    }
-    }
+public class Faktöriyel {
+public static void main (String[] args) {
+    int faktöriyel = 5*4*3*2*1;
+    System.out.println("5*4*3*2*1") ;
+    System.out.println("5!= " + faktöriyel);
+     }
+}
+
+
