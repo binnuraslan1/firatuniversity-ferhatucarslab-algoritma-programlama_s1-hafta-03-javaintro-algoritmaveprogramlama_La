51301437d5aa1e4cffba8fd7790982dc3241d3a9
binnuraslan1 <binnuraslan888@gmail.com>
2025-10-22 21:15:28 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class UcMesaj {
+    public static void main (String [] args) {
+    System.out.println("Java'ya Hos Geldiniz");
+    System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+    System.out.println("Programlama Eğlencelidir");
+
+}
+}
