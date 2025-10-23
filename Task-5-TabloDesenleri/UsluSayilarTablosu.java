56710509c58a3093afe35069862ca1e9f6d7d3d6
binnuraslan1 <binnuraslan888@gmail.com>
2025-10-23 11:37:56 +0300

    UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,26 @@
+public class UsluSayilarTablosu {
+    public static void main(String[] args) {
+     System . out . println ( " a a ^2 a ^3 " ) ;
+     System . out . println ( " - - - - - - - - - - - - - - - - - " ) ;
+     int a1 = 1;
+    System . out . println ( a1 + " "
+    + ( a1 * a1 ) + " " + ( a1 * a1 * a1 ) )    
+    ;
+     int a2 = 2;
+    System . out . println ( a2 + " "
+    + ( a2 * a2 ) + " " + ( a2 * a2 * a2 ) )    
+    ;
+     int a3 = 3;
+    System . out . println ( a3 + " "
+    + ( a3 * a3 ) + " " + ( a3 * a3 * a3 ) )    
+    ;
+     int a4 = 4;
+    System . out . println ( a4 + " "
+    + ( a4 * a4 ) + " " + ( a4 * a4 * a4 ) )    
+    ;
+     int a5 = 5;
+    System . out . println ( a5 + " "
+    + ( a5 * a5 ) + " " + ( a5 * a5 * a5 ) )    
+    ;
+}
+}
