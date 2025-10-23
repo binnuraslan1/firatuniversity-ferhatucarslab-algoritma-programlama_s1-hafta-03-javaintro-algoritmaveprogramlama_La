5a79997be66eaac6b9285e0ed9275ae0ca73f83f
binnuraslan1 <binnuraslan888@gmail.com>
2025-10-23 11:05:38 +0300

    AsciiArtHarf

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,12 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        System.out.println(" ********   ");
+        System.out.println(" **     **  ");
+        System.out.println(" **     **  ");
+        System.out.println(" ********   ");
+        System.out.println(" **     **  ");
+        System.out.println(" **     **  ");
+        System.out.println(" **     **  ");
+        System.out.println(" ********   ");
+    }
+}
