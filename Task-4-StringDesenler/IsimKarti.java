cd612953111bf2cb07fd1f8c02307b3ac6f9a81d
binnuraslan1 <binnuraslan888@gmail.com>
2025-10-23 10:55:37 +0300

    IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main (String [] args) {
+
+        String ad = "Binnur";
+        String soyad = "Aslan";
+        String ogrNo = "250541084";
+        String bolum = "Yazilim Muhendisligi";
+
+        System.out.println("= = = = = = = = = = = = = = = = = = = ");
+        System.out.println("OGRENCI BILGILERI");
+        System.out.println("= = = = = = = = = = = = = = = = = = = ");
+        System.out.println("Ad: " + ad);
+        System.out.println("Soyad: " + soyad);
+        System.out.println("Ogrenci No: " + ogrNo);
+        System.out.println("Bolum: " + bolum);
+    }
+ }
