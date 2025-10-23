f9b3fef8c99174e1a9d80d1a147cee8efd6dddc3
binnuraslan1 <binnuraslan888@gmail.com>
2025-10-23 11:18:07 +0300

    HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,15 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+        // Tablo başlığı
+        System.out.println("Ürün\t\tMiktar\tFiyat\tTutar");
+        System.out.println("----\t\t-----\t-----\t-----");
+        // Ürünler
+        System.out.println("Elma\t\t3\t5.0\t15.0");
+        System.out.println("Ekmek\t\t2\t2.5\t5.0");
+        System.out.println("Süt\t\t1\t8.0\t8.0");
+        System.out.println("Peynir\t\t1\t25.0\t25.0");
+        System.out.println("----\t\t-----\t-----\t-----");
+        // Toplam
+        System.out.println("Toplam\t\t\t\t53.0");
+    }
+}
