d946bbff3bd0f68f1b761e7f914beb4b8e16e856
binnuraslan1 <binnuraslan888@gmail.com>
2025-10-23 10:20:21 +0300

    DaireHesap

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,11 @@
+public class DaireHesap {
+    public static void main (String [] args) {
+        double pi = 3.14159;
+        double r = 5.5;
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+        System.out.println("Yaricap: " + r);
+        System.out.println("Alan: " + alan);
+        System.out.println("Cevre: " + cevre);
+    }
+}
